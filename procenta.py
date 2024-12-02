6507c88b9e2b3210012b4c907b55da25e58bc183
Trenclik <trenclikb1@gmail.com>
2024-12-03 00:24:45 +0100

    Update procenta.py

--- procenta.py
+++ procenta.py
@@ -1,9 +1,26 @@
+import warnings as w
+from inspect import currentframe
 class Procenta():
-    def vstup():
-        text = "bagriel je negr cierny gazovany napoj dingoo o smaku limonky"
-        return text
+    """třída pro náhodný výběr textu pro dešifrování podle zadaných procent
+    slice:
+        
+    """
+    
+    def custom_showwarning(message, category, filename, lineno, file=None, line=None):
+        print(f"\033[1;30;43mVAROVÁNÍ: {message}\033[0m")
+    w.showwarning = custom_showwarning
+    
+    def slice(text: str):
+        """Rozdělí text na jednotlivá slova v listu. Nevrací mezery.
 
-    def slice(text):
+        Vstup:
+            text (string): text k rozdělení
+
+        Výstup:
+            list: list se stringy slov oddělených od sebe
+        """
+        if text == "":
+            w.warn(f"Prázdný vstup do funkce {currentframe().f_code.co_name}. Funkce by měla stále fungovat ale výstup bude prázdný.")
         sliced = []
         slovo = ""
         for i in text + " ":
@@ -13,3 +30,24 @@ class Procenta():
                 sliced.append(slovo)
                 slovo = ""
         return sliced
+    
+    def vyber(procenta: int):
+        """Náhodně vybere určité procento slov z listu a vrátí jiný list s vybranými slovy.
+        Zapamatuje si dekódovaná slova aby nedošlo k chybám v dešifrování.
+        
+        Vstup:
+            procenta (int): procento slov která budou zakódována
+        Výstup:
+            slova (list): náhodný výběr slov
+        """
+        if not isinstance(procenta, int):
+            raise TypeError("Vstup musí být celé číslo!")
+        if procenta == 0:
+            w.warn("Vstupní hodnota nastavena na nulu. Funkce by měla stále fungovat ale výstup bude prázdný. Doporučuji nastavit alespoň na 10%")
+        if procenta <0:
+            w.warn("Nelze vybrat číslo menší než 0! Měním na 0%.")
+            procenta = 0
+        if procenta >100:
+            w.warn("Nelze vybrat číslo větší než 100! Měním na 100%")
+            procenta = 100
+        #dodělat zbytek logiky
